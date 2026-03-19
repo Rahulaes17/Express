e391a608555a608a7de9c214847dad1ba052869e
rahulaes17 <rahulaes1704@gmail.com>
2026-03-19 22:10:36 +0530

    learning how to add static files

---
+++ Serving Static files/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/search', (req, res) => {
+    let query = req.query.q
+    let name = req.query.name
+    let location = req.query.location
+    
+    res.send(`You searched for: ${query}<br>Your name is: ${name}<br>The location is: ${location}`)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
